a05e00936f522d3f12bcabf2df6071269be01a7f
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-04-06 21:10:04 -0300

    Projeto finalizado

---
+++ src/routes/tags.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+
+const TagsController = require("../controllers/TagsController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+
+const tagsRouters = Router()
+
+const tagsController = new TagsController()
+
+tagsRouters.get("/", ensureAuthenticated, tagsController.index);
+
+module.exports = tagsRouters;
